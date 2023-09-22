44666d07fc02b257f6620284427baf0afd49eeeb
AlexaBenavides19 <alexa.senacopia@gmail.com>
2023-09-22 08:30:41 -0500

    gatito2

--- src/index.js
+++ src/index.js
@@ -14,12 +14,6 @@ app.use((req, res, next) => {
   });
 
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://apifront.onrender.com');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
 
 // Puerto
 const port = process.env.PORT || 3000;
