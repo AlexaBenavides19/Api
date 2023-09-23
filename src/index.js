c1047e9df112f723bce6d463c49cf3444ea4ab00
AlexaBenavides19 <alexa.senacopia@gmail.com>
2023-09-22 21:34:55 -0500

    uwu

--- src/index.js
+++ src/index.js
@@ -13,6 +13,12 @@ app.use((req, res, next) => {
     next();
   });
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://frontend-uzmu.onrender.com');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
 
 // Puerto
